92584d9a676adde8393be60d99088036130e5c3d
mullerow <maximilian.kohler@posteo.de>
2024-06-18 16:37:28 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script>
+import { store } from '@/stores/store.js'
+export default {
+  data() {
+    return {
+      store: store()
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div class="tile-container">
+      <div v-for="tile of store.tileData.data" :key="tile" class="single-tile">{{ tile }}</div>
+    </div>
+  </main>
+</template>
+
+<style>
+.tile-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 400px;
+}
+.single-tile {
+  width: 50px;
+  height: 50px;
+  background-color: blanchedalmond;
+  border: 2px solid black;
+  text-align: center;
+}
+</style>
