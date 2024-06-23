3d575a72a923d1337eac94cbffabf2fbc1272d0e
mullerow <maximilian.kohler@posteo.de>
2024-06-23 09:31:18 +0200

    chore: add initialCalcOfCoordinates function

--- src/stores/store.js
+++ src/stores/store.js
@@ -4,10 +4,25 @@ import { defineStore } from 'pinia'
 export const store = defineStore('Store', {
   state: () => ({
     tileData: {
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+      number: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+        26, 27, 28, 29, 30
+      ],
+      tileCoordinates: []
+    },
+    temporayData: {
+      amountRows: 6,
+      amountColumns: 5
     }
   }),
   actions: {
-    testAction() {}
+    initialCalcOfCoordinates() {
+      for (let x = 1; x <= this.temporayData.amountRows; x++) {
+        for (let y = 1; y <= this.temporayData.amountColumns; y++) {
+          this.tileData.tileCoordinates.push([x, y])
+        }
+      }
+      console.log('this.tileData.tileCoordinates', this.tileData.tileCoordinates)
+    }
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,71 @@
+<template>
+  <main>
+    <div class="background-image-tiles">
+      <div class="tile-container">
+        <div
+          @mouseover="handleMouseHover($event, index)"
+          @mouseleave="handleMouseLeave($event, index)"
+          v-for="(tile, index) of store.tileData.number"
+          :key="index"
+          :data-id="index"
+          :class="{
+            'single-tile': true,
+            hovered: hoveredTile === index,
+            leaveTile: tileHoverLeave === index
+          }"
+        >
+          {{ tile }}
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
 <script>
 import { store } from '@/stores/store.js'
 export default {
   data() {
     return {
-      store: store()
+      store: store(),
+      hoveredTile: null,
+      tileHoverLeave: null,
+      neighbourTiles: []
     }
+  },
+  methods: {
+    handleMouseHover(e, index) {
+      this.hoveredTile = index
+      this.tileHoverLeave = null
+      const tileId = e.target.getAttribute('data-id')
+      console.log('Id:', tileId)
+      // berechne alle nummern der benachtbarten Kacheln
+      for (let y = -6; y < 7; y += 6) {
+        for (let x = -1; x < 2; x++) {
+          let neighbourNumber = Number(tileId) + y + x
+          if (neighbourNumber > 0 && neighbourNumber < this.store.tileData.number.length) {
+            this.neighbourTiles.push(neighbourNumber)
+          }
+        }
+      }
+      console.log('this.neighbourTiles', this.neighbourTiles)
+    },
+
+    handleMouseLeave(e, index) {
+      this.neighbourTiles = []
+      setTimeout(() => {
+        this.tileHoverLeave = index
+      }, 1000),
+        setTimeout(() => {
+          this.hoveredTile = null
+        }, 100)
+    }
+  },
+  created() {
+    console.log('habs getan')
+    this.store.initialCalcOfCoordinates()
   }
 }
 </script>
-
-<template>
-  <main>
-    <div class="tile-container">
-      <div v-for="tile of store.tileData.data" :key="tile" class="single-tile">{{ tile }}</div>
-    </div>
-  </main>
-</template>
 
 <style>
 .tile-container {
@@ -32,16 +82,41 @@ export default {
   text-align: center;
 }
 
-.single-tile:hover {
+.single-tile.hovered {
   background-color: goldenrod;
-  animation: rotate-tile 1s forwards;
+  animation: rotate-tile-forward 1s forwards;
 }
 
-@keyframes rotate-tile {
+.single-tile.leaveTile {
+  background-color: rgb(231, 200, 120);
+  animation: rotate-tile-backwards 0.5s backwards;
+}
+.background-image-tiles {
+  width: 350px;
+  height: 290px;
+  background-color: goldenrod;
+  z-index: -1;
+}
+
+@keyframes rotate-tile-forward {
   0% {
   }
   100% {
     transform: rotateY(180deg);
+  }
+}
+@keyframes rotate-tile-backwards {
+  0% {
+  }
+  100% {
+    transform: rotateY(-180deg);
+  }
+}
+@keyframes rotate-50-percent {
+  0% {
+  }
+  100% {
+    transform: rotateY(90deg);
   }
 }
 </style>
